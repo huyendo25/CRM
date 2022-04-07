3a2dd4ca4be9b2409386858bb8c09cadf9b73b5e
Bảo Bảo <huyenjy@gmail.com>
2022-04-07 15:17:52 +0700

    update search stage 2nd

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from pickle import NONE
-from flask import Flask, request, redirect, url_for , jsonify , send_file ,send_from_directory
+from flask import Flask, request, redirect, url_for, jsonify, send_file, send_from_directory
 from werkzeug.utils import secure_filename
 from crm import *
 import json
+
 
 class DataModel:
     def __init__(self, result, message, item):
@@ -10,29 +11,35 @@ class DataModel:
         self.message = message
         self.item = item
 
+
 class ErrorModel:
-    def __init__(self, result, message,item):
+    def __init__(self, result, message, item):
         self.result = result
         self.message = message
         self.item = item
+
 
 class ResponseModel:
     def __init__(self, data, error):
         self.data = data
         self.error = error
 
+
 UPLOAD_FOLDER = 'static/uploads'
-ALLOWED_EXTENSIONS = {'docx','doc','pdf'}
+ALLOWED_EXTENSIONS = {'docx', 'doc', 'pdf'}
 
 app = Flask(__name__, static_url_path='/static')
-app.config['upload_folder'] = UPLOAD_FOLDER 
+app.config['upload_folder'] = UPLOAD_FOLDER
+
 
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+
 input_file = {}
 dic_new_text = {}
+
 
 @app.route('/upload_file', methods=['GET', 'POST'])
 def upload_file():
@@ -45,13 +52,14 @@ def upload_file():
             filename = secure_filename(file.filename)
             file_name, file_end = os.path.splitext(filename)
             path_file_name = file_name + '_' + sess_id
-            path_file = os.path.join(app.config['upload_folder'], path_file_name )
+            path_file = os.path.join(
+                app.config['upload_folder'], path_file_name)
             if not os.path.exists(path_file):
                 os.makedirs(path_file)
             input_file[sess_id] = os.path.join(path_file, filename)
             file.save(input_file[sess_id])
             img_org_base64 = start(input_file[sess_id])
-            item = {"sess_id": sess_id , "image" : img_org_base64}
+            item = {"sess_id": sess_id, "image": img_org_base64}
             data = DataModel(True, " Xử lí file thành công ", item)
         if error is not None:
             error = vars(error)
@@ -68,6 +76,7 @@ def upload_file():
         response = ResponseModel(data, error)
     return json.dumps(vars(response))
 
+
 @app.route('/search', methods=['GET', 'POST'])
 def search():
     error = None
@@ -76,7 +85,7 @@ def search():
     if request.method == 'POST':
         key = request.form['text_change']
         try:
-            img_base64, countKey = stage2(input_file[sess_id],key)
+            img_base64, countKey = stage2(input_file[sess_id], key)
         except:
             item = {"sess_id": sess_id}
             data = DataModel(False, " Không tìm thấy tập tin! ", item)
@@ -97,7 +106,8 @@ def search():
                     data = vars(data)
                 response = ResponseModel(data, error)
             else:
-                item = {"sess_id": sess_id, "number_text": countKey, "image":  img_base64}
+                item = {"sess_id": sess_id,
+                        "number_text": countKey, "image":  img_base64}
                 data = DataModel(True, " Ảnh trả về ", item)
                 if error is not None:
                     error = vars(error)
@@ -105,6 +115,7 @@ def search():
                     data = vars(data)
                 response = ResponseModel(data, error)
         return json.dumps(vars(response))
+
 
 @app.route('/replace_file', methods=['GET', 'POST'])
 def replace():
@@ -117,8 +128,10 @@ def replace():
         numberList.append(content['index'])
     key = contents[0]['name']
     value = contents[0]['replace_with']
-    img_org_base64,output_file = stage3(input_file[sess_id],key,value,numberList)
-    item = {"sess_id" : sess_id , "img_org_base64": img_org_base64,"url": output_file}
+    img_org_base64, output_file = stage3(
+        input_file[sess_id], key, value, numberList)
+    item = {"sess_id": sess_id,
+            "img_org_base64": img_org_base64, "url": output_file}
     data = DataModel(True, "File thay đổi thành công ", item)
     if error is not None:
         error = vars(error)
@@ -126,6 +139,7 @@ def replace():
         data = vars(data)
     response = ResponseModel(data, error)
     return json.dumps(vars(response))
+
 
 '''@app.route("/delete" ,methods=['GET', 'POST'])
 def delete():
@@ -151,9 +165,11 @@ def delete():
     response = ResponseModel(data, error)
     return json.dumps(vars(response))'''
 
+
 @app.route("/static/<path:path>")
 def static_dir(path):
     return send_from_directory("static", path)
 
+
 if __name__ == "__main__":
-    app.run (host='0.0.0.0', port=4000)
+    app.run(host='0.0.0.0', port=4000)
